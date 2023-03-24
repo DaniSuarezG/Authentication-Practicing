c9f5d4a51b3a14f67cc8192fb279efbf8d076dc4
DaniSuarezG <DanielSuarezGalvez@Gmail.com>
2023-03-24 10:42:55 +0000

    GENERAL - Added json parser

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const api = express()
 
 api.use(morgan('dev'))
+api.use(express.json())
 
 api.listen(process.env.PORT, (err) => {
     if (err) throw new Error(`Cannot initialize Express on port ${process.env.PORT}`)
