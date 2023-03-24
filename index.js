015d9c32b4945ef49fcb9530f23535c053450c1f
DaniSuarezG <DanielSuarezGalvez@Gmail.com>
2023-03-24 11:01:45 +0000

    GENERAL - Sequelize set up

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const morgan = require('morgan')
 const express = require('express')
 const api = express()
 
+const dbConn = require('./db')
+
 api.use(morgan('dev'))
 api.use(express.json())
 
-api.listen(process.env.PORT, (err) => {
-    if (err) throw new Error(`Cannot initialize Express on port ${process.env.PORT}`)
+api.listen(process.env.PORT, async (err) => {
+    if (err) throw new Error(`Cannot initialize Express on port ${process.env.PORT}:`, err)
 
+    try {
+        await dbConn.authenticate()
+        console.info(`Connected to DB ${process.env.DB_NAME}.`)
+    } catch (error) {
+        throw new Error('Cannot connect to DB:', error)
+    }
     console.info(`Netflix API listening on port ${process.env.PORT}`)
 })
